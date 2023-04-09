7862d4e4ac0d2777bbbc2f1eddf836f1eabfd99a
PriyankaDashputre <dashputre.priyanka@gmail.com>
2023-04-09 12:30:45 -0700

    arrow functions

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (nums, nums2) => {
+    nums = 2000;
+    nums2 = 100;
+    return nums / nums2;
+};
+
+const square = (num) => num * num;
+square(5);
+
+const add = (num1, num2) => num1 + num2;
+add(6,6);
